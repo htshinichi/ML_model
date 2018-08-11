be93a4a0db730f84678ec301af101fc87a29d9b5
htshinichi <33441642+htshinichi@users.noreply.github.com>
2018-08-11 12:49:08 +0800

    Create DT_SIKA_test.py

---
+++ DecisionTree/DT_SIKA_test.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 11 12:43:13 2018
+
+@author: htshi
+"""
+import pandas as pd
+import DecisionTREE
+import DrawDecisionTREE
+sika = pd.read_csv("sika.csv")
+sika_feature=sika.columns.values.tolist()[:8]
+fealabel = ['色泽', '根蒂', '敲击', '纹理', '脐部', '触感', '密度', '含糖率']
+DT_model = DecisionTREE.DecisionTree()
+DDT = DrawDecisionTREE.DrawDecisionTree()
+sika_model = DT_model.create_tree_C45(sika,sika_feature)
+print(sika_model)
+DDT.createPlot(sika_model)
