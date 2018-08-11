c12574487682abeff627c85f79d7df7b1367c9ca
htshinichi <33441642+htshinichi@users.noreply.github.com>
2018-08-11 19:16:08 +0800

    Update KnnClassifier.py

--- KNN/KnnClassifier.py
+++ KNN/KnnClassifier.py
@@ -1,30 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 11 19:14:25 2018
+
+@author: htshinichi
+"""
 import numpy as np
-import pandas as pd 
 from collections import Counter
+
+#---------------------KNN模型定义----------------------#
 class KNN():
     #--------------初始化参数k(最近邻个数)------------#
     def __init__(self,k=5):
         self.k = k
     #------------------计算欧氏距离-----------------#
-    def euclidean_distance(self,one_sample,X_train):#计算一个样本与训练中所有样本的欧氏距离的平方
+    def euclidean_distance(self,one_sample,X_train):#计算一个测试样本与所有训练样本的欧氏距离的平方
+        #将测试样本变成只有1行
         one_sample = one_sample.reshape(1,-1)
         X_train = X_train.reshape(X_train.shape[0],-1)
-        distances = np.power(np.tile(one_sample,(X_train.shape[0],1)) - X_train,2).sum(axis=1)
-        return distances
+        #将测试样本沿y轴复制，使其行数等于训练样本行数
+        X_test = np.tile(one_sample,(X_train.shape[0],1))
+        #计算测试样本与每一个训练样本的欧氏距离
+        distance_arr = np.sqrt(np.power(X_test - X_train,2).sum(axis=1))
+        return distance_arr
     #-------------获取最近k个近邻的标签------------#
     def get_k_neighbor_labels(self,distances,y_train,k):
         k_neighbor_labels = []
+        #按照距离排序选择前k个获取标签
         for distance in np.sort(distances)[:k]:
             label = y_train[distances==distance]
             k_neighbor_labels.extend(label)
+        #将返回k个近邻标签列表转为数组，并只有一行
         return np.array(k_neighbor_labels).reshape(-1, )
     #-------------投票得到某样本的类别------------#
     def vote(self,one_sample,X_train,y_train,k):
-        distances = self.euclidean_distance(one_sample,X_train)
+        #获取测试集所有样本与训练集欧氏距离的数组
+        Distances = self.euclidean_distance(one_sample,X_train)
         y_train = y_train.reshape(y_train.shape[0],1)
-        k_neighbor_labels = self.get_k_neighbor_labels(distances,y_train,k)
+        #获取最近k个近邻的标签数组
+        self.knn_labels_arr = self.get_k_neighbor_labels(Distances,y_train,k)
+        #初始化标签和数
         find_label,find_count = 0,0
-        for label,count in Counter(k_neighbor_labels).items():
+        #使用Counter统计每个标签出现次数，label为标签，count为标签出现次数
+        for label,count in Counter(self.knn_labels_arr).items():
             if count > find_count:
                 find_count = count
                 find_label = label
@@ -32,6 +49,7 @@ class KNN():
     #-------------------预测---------------------#
     def predict(self,X_test,X_train,y_train):
         y_pred = []
+        #获取每个测试样本的预测标签
         for sample in X_test:
             label = self.vote(sample,X_train,y_train,self.k)
             y_pred.append(label)
