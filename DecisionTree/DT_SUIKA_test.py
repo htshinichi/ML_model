e8cc380cd70696264f3a3a850dc453c7ff4a9ea4
htshinichi <33441642+htshinichi@users.noreply.github.com>
2018-08-17 10:08:36 +0800

    Update DT_SUIKA_test.py

--- DecisionTree/DT_SUIKA_test.py
+++ DecisionTree/DT_SUIKA_test.py
@@ -2,16 +2,16 @@
 """
 Created on Sat Aug 11 12:43:13 2018
 
-@author: htshi
+@author: htshinichi
 """
 import pandas as pd
 import DecisionTREE
 import DrawDecisionTREE
-suika = pd.read_csv("suika3.0.csv")
-suika_feature=suika.columns.values.tolist()[:8]
+sika = pd.read_csv("sika3.0.csv")
+sika_feature=sika.columns.values.tolist()[:8]
 fealabel = ['色泽', '根蒂', '敲击', '纹理', '脐部', '触感', '密度', '含糖率']
-DT_model = DecisionTREE.DecisionTree()
+DT_model = DecisionTREE.DecisionTree(split='C45')
 DDT = DrawDecisionTREE.DrawDecisionTree()
-suika_model = DT_model.create_tree_C45(suika,suika_feature)
-print(suika_model)
-DDT.createPlot(suika_model)
+sika_model = DT_model.create_tree(sika,sika_feature)
+print(sika_model)
+DDT.createPlot(sika_model)
