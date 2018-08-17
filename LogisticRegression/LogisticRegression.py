39d0bd37986593eb0c78c7f82f30d5b4687eb0c3
htshinichi <33441642+htshinichi@users.noreply.github.com>
2018-08-17 13:59:09 +0800

    Update LogisticRegression.py
    添加了随机梯度下降，并且在初始化类时可以选择使用批量梯度下降或是随机梯度下降

--- LogisticRegression/LogisticRegression.py
+++ LogisticRegression/LogisticRegression.py
@@ -6,22 +6,39 @@ Created on Thu Aug 16 19:07:45 2018
 """
 import numpy as np  
 class LogisticRegression():
-    def __init__(self,n_iter = 100,eta = 0.0001):
+    def __init__(self,n_iter = 100,eta = 0.0001,gd = 'bgd'):
         self.n_iter = n_iter
         self.eta = eta
+        self.gd = gd
+        
     def sigmoid(self,fx):
         return 1/(1+np.exp(-1 * fx))
+    
     def fit(self,TrainData):
         y_label = np.array(TrainData.label)#获取真实标签集合
         datanum = len(TrainData)#获取数据集大小
         featnum = len(TrainData.columns)-1#获取特征数量
-        data = np.array(TrainData[TrainData.columns.tolist()[0:featnum]])#datanum×featnum
-        data_T = data.transpose()#数据大小为featnum×datanum
         self.weights = np.zeros((1,featnum))#初始化权重，权重大小为1×featnum
-        for n in range(self.n_iter):
-            hx = self.sigmoid(np.dot(self.weights,data_T))#预测值，即h(x)=1/(1+e^(-wx))
-            self.weights = self.weights - self.eta * np.dot((hx - y_label),data)/datanum#即weights = weights - eta*(h(x)-y)*x/datanum
-        return self.weights
+        
+        #批量梯度下降
+        if self.gd == 'bgd':
+            data = np.array(TrainData[TrainData.columns.tolist()[0:featnum]])#datanum×featnum
+            data_T = data.transpose()#数据大小为featnum×datanum
+            for n in range(self.n_iter):
+                hx = self.sigmoid(np.dot(self.weights,data_T))#预测值，即h(x)=1/(1+e^(-wx))
+                self.weights = self.weights - self.eta * np.dot((hx - y_label),data)/datanum#即weights = weights - eta*(h(x)-y)*x/datanum
+            return self.weights
+        
+        #随机梯度下降
+        if self.gd == 'sgd':
+            for n in range(self.n_iter):
+                x = random.randint(0,datanum-1)
+                datax = np.array(TrainData[TrainData.columns.tolist()[0:featnum]])[x]
+                datax_T = datax.transpose()
+                hxx = self.sigmoid(np.dot(self.weights,datax_T))
+                self.weights = self.weights - self.eta * (hxx - y_label[x]) * datax#即weights = weights - eta*(h(x)-y)*x/datanum
+            return self.weights
+                
     def predict(self,testData):
         flag = np.dot(self.weights,testData)
         if flag > 0:
@@ -29,6 +46,7 @@ class LogisticRegression():
         else:
             pred = 0
         return pred
+    
     def accuracy(self,TestData):
         num = 0
         for i in range(len(TestData)):
