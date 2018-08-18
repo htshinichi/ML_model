7de9a588749493e6d4bd485fad8777c1bb79939c
htshinichi <33441642+htshinichi@users.noreply.github.com>
2018-08-18 20:33:35 +0800

    Update LogisticRegression.py

--- LogisticRegression/LogisticRegression.py
+++ LogisticRegression/LogisticRegression.py
@@ -4,15 +4,16 @@ Created on Thu Aug 16 19:07:45 2018
 
 @author: htshinichi
 """
+import random
 import numpy as np  
 class LogisticRegression():
     def __init__(self,n_iter = 100,eta = 0.0001,gd = 'bgd'):
-        self.n_iter = n_iter
-        self.eta = eta
-        self.gd = gd
+        self.n_iter = n_iter #迭代次数，默认为100
+        self.eta = eta #学习率，默认为0.0001
+        self.gd = gd #学习方式，默认为批量梯度下降
         
     def sigmoid(self,fx):
-        return 1/(1+np.exp(-1 * fx))
+        return 1/(1+np.exp(-1 * fx)) #对数几率函数
     
     def fit(self,TrainData):
         y_label = np.array(TrainData.label)#获取真实标签集合
@@ -38,7 +39,8 @@ class LogisticRegression():
                 hxx = self.sigmoid(np.dot(self.weights,datax_T))
                 self.weights = self.weights - self.eta * (hxx - y_label[x]) * datax#即weights = weights - eta*(h(x)-y)*x/datanum
             return self.weights
-                
+    
+    #预测单个样本类别            
     def predict(self,testData):
         flag = np.dot(self.weights,testData)
         if flag > 0:
@@ -47,6 +49,7 @@ class LogisticRegression():
             pred = 0
         return pred
     
+    #计算准确率
     def accuracy(self,TestData):
         num = 0
         for i in range(len(TestData)):
